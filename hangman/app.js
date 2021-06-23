e0064ed318943bbc3c60f4c053d3f2b6a18374b6
nandita3 <nanditas.03@gmail.com>
2021-06-23 23:21:48 +0530

    started working on JS file, added a comment in CSS file

---
+++ hangman/app.js
@@ -0,0 +1,8 @@
+const wrong_letters = document.getElementById("wrong-letters");
+const word = document.getElementById("word");
+const popup = document.getElementById("popup-container");
+const final_msg = document.getElementById("final-msg");
+const playBtn = document.getElementById("play-button");
+const notificationCont = document.getElementById("notification-container");
+
+const figureParts = document.querySelectorAll("figure-part");
