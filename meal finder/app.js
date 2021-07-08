8dfbd7b3138cbccf1c8fd198ceee85a1f602f138
nandita3 <nanditas.03@gmail.com>
2021-07-08 10:48:03 +0530

    completed project

--- meal finder/app.js
+++ meal finder/app.js
@@ -59,7 +59,7 @@ const searchMeal = (e) => {
 
 
 
-//fetch meal by ID
+//fetch meal details by ID
 const getMealById = (mealID) => {
     fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}')
     .then(res => res.json())
@@ -72,10 +72,56 @@ const getMealById = (mealID) => {
     });
 }
 
+//Fetch Random meal
+const getRandomMeal = () => {
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
 
-//Add recipe to DOM
+    fetch('www.themealdb.com/api/json/v1/1/randomselection.php')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        const meal = data.meals[0];
+
+        addMealToDom(meal);
+    })
+}
+
+
+//Add meal details to DOM
 const addMealToDom = () => {
+    const ingredients = [];
 
+// Below we are creating an array of ingredients and their quantities in the format ingredient - quantity, max number of ingredients is 20 for any meal
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `;
 }
 
 
@@ -84,7 +130,9 @@ const addMealToDom = () => {
 
 submit.addEventListener('submit', searchMeal);
 
+random.addEventListener('click', getRandomMeal);
 
+// event listener to get hold of the mealID attribute (that is equal to idMeal) from the mealsEl element. That mealID will be passed in a function that will be used to fetch the meal details by ID (there is an API that gives meal details by its ID)
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
         console.log(item);
